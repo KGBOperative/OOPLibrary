b20b804883fdcb53885966c2b562ab54765255c7
Marshall Jankovsky <jankovsk@cwolf.cs.sonoma.edu>
2013-10-11 10:54:42 -0700

    Prototyping application

---
+++ P1app.cpp
@@ -0,0 +1,22 @@
+// file: P1app.cpp
+// Authors: Marshall Jankovsky, 
+
+#include <iostream>
+#include "Library.h"
+#include "Date.h"
+
+using namespace std;
+
+Library loadLib (string fName);
+void saveLib (Library L);
+void addMember (Library L);
+void removeMember (Library L);
+void addAsset (Library L);
+void removeAsset (Library L);
+void 
+void makeReport(Library L);
+
+int main(){
+
+return 0;			
+}
